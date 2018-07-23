a661d5312e848d46c678f321a52aba725508df15
kaysal <salawu@gmail.com>
2018-07-23 17:31:54 +0100

    re-arranged output resources

--- hybrid/svpc/1-gcp-org-iam/output.tf
+++ hybrid/svpc/1-gcp-org-iam/output.tf
@@ -3,21 +3,21 @@ output "netsec_folder_id" {
   value = "${google_folder.netsec_folder.id}"
 }
 
-output "netsec_host_project_id" {
-  value = "${google_project.netsec_host_project.id}"
-}
-
 output "prod_folder_id" {
   value = "${google_folder.prod_folder.id}"
 }
 
-# project ids
-output "prod_service_project_id" {
-  value = "${google_project.prod_service_project.id}"
-}
-
 output "test_folder_id" {
   value = "${google_folder.test_folder.id}"
+}
+
+# project ids
+output "netsec_host_project_id" {
+  value = "${google_project.netsec_host_project.id}"
+}
+
+output "prod_service_project_id" {
+  value = "${google_project.prod_service_project.id}"
 }
 
 output "test_service_project_id" {

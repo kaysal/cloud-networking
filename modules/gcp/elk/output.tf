32e0fdefe186a95d69d682d10314546e5a24b104
Kayode Salawu <salawu@google.com>
2019-04-03 22:32:06 +0100

    removed modules directory

--- modules/gcp/elk/output.tf
+++
@@ -1,8 +0,0 @@
-output "pubsub_topic" {
-  description = "PubSub topic name"
-  value       = "${google_pubsub_topic.logstash_input_dev.name}"
-}
-
-output "elk_nat_ip" {
-  value = "${google_compute_instance.elk_stack.network_interface.0.access_config.0.nat_ip}"
-}

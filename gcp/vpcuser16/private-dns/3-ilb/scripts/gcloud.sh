d99764dec269d86012853a79411e3949f76d423b
kaysal <salawu@google.com>
2019-04-06 17:12:03 +0100

    removed vpcuser16 dir

--- gcp/vpcuser16/private-dns/3-ilb/scripts/gcloud.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gcloud dns managed-zones create gcp-cloudtuple\
---project=host-project-f0 \
---description= \
---dns-name=gcp.cloudtuple.com. \
---visibility=private \
---networks vpc

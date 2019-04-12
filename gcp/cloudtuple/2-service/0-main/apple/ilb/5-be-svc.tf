e22e445554d559d2c384690f0dabaab7abe31736
kaysal <salawu@google.com>
2019-04-11 22:11:01 -0700

    renamed TF file names

---
+++ gcp/cloudtuple/2-service/0-main/apple/ilb/5-be-svc.tf
@@ -0,0 +1,12 @@
+# internal load balancing - backend services
+resource "google_compute_region_backend_service" "prod_ilb" {
+  name     = "${var.main}prod-be-svc"
+  region   = "europe-west1"
+  protocol = "TCP"
+
+  backend {
+    group = "${google_compute_region_instance_group_manager.prod_mig.instance_group}"
+  }
+
+  health_checks = ["${google_compute_health_check.ilb_health.self_link}"]
+}
--- gcp/cloudtuple/2-service/0-main/apple/ilb/6-fwd-rule.tf
+++ gcp/cloudtuple/2-service/0-main/apple/ilb/6-fwd-rule.tf
@@ -1,16 +1,3 @@
-# internal load balancing - backend services
-resource "google_compute_region_backend_service" "prod_ilb" {
-  name     = "${var.main}prod-be-svc"
-  region   = "europe-west1"
-  protocol = "TCP"
-
-  backend {
-    group = "${google_compute_region_instance_group_manager.prod_mig.instance_group}"
-  }
-
-  health_checks = ["${google_compute_health_check.ilb_health.self_link}"]
-}
-
 # internal forwarding rules
 resource "google_compute_forwarding_rule" "prod_ilb_fwd_rule" {
   provider = "google-beta"

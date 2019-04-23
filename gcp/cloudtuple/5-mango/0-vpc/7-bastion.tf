6a5efd34fba1d1582e3d436c687812d3ff642389
kaysal <salawu@google.com>
2019-04-23 12:23:01 +0100

    dns zone name change for bastion host

--- gcp/cloudtuple/5-mango/0-vpc/7-bastion.tf
+++ gcp/cloudtuple/5-mango/0-vpc/7-bastion.tf
@@ -21,7 +21,7 @@ module "bastion" {
 resource "google_dns_record_set" "bastion_public" {
   project      = "${data.terraform_remote_state.host.host_project_id}"
   managed_zone = "${data.google_dns_managed_zone.public_host_cloudtuple.name}"
-  name         = "bastion.mango.${data.google_dns_managed_zone.public_host_cloudtuple.dns_name}"
+  name         = "bastion.main.mango.${data.google_dns_managed_zone.public_host_cloudtuple.dns_name}"
   type         = "A"
   ttl          = 300
   rrdatas      = ["${module.bastion.bastion_nat_ip}"]
@@ -30,7 +30,7 @@ resource "google_dns_record_set" "bastion_public" {
 resource "google_dns_record_set" "bastion_private" {
   project      = "${data.terraform_remote_state.mango.mango_project_id}"
   managed_zone = "${google_dns_managed_zone.private_mango_cloudtuple.name}"
-  name         = "bastion.${google_dns_managed_zone.private_mango_cloudtuple.dns_name}"
+  name         = "bastion.main.${google_dns_managed_zone.private_mango_cloudtuple.dns_name}"
   type         = "A"
   ttl          = 300
   rrdatas      = ["${module.bastion.bastion_private_ip}"]

79c7338fdb17ea460ae3515ff7b6a1e32d0da969
kaysal <salawu@google.com>
2019-04-23 04:54:59 +0100

    internet gateway short name used

--- gcp/cloudtuple/5-mango/0-vpc/3-routes.tf
+++ gcp/cloudtuple/5-mango/0-vpc/3-routes.tf
@@ -3,6 +3,6 @@ resource "google_compute_route" "private_googleapis" {
   description      = "Route to default gateway for PGA"
   dest_range       = "199.36.153.4/30"
   network          = "${google_compute_network.vpc.name}"
-  next_hop_gateway = "global/gateways/default-internet-gateway"
+  next_hop_gateway = "default-internet-gateway"
   priority         = 1000
 }

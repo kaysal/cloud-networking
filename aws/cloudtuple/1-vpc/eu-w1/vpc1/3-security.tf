4135da21756dd1f4aa05b0bf64dd62d469005d6b
kaysal <salawu@google.com>
2019-05-05 04:08:01 +0100

    removed 0.0.0.0/0 and added rfc1918 and cgn ip blocks

--- aws/cloudtuple/1-vpc/eu-w1/vpc1/3-security.tf
+++ aws/cloudtuple/1-vpc/eu-w1/vpc1/3-security.tf
@@ -21,21 +21,35 @@ resource "aws_security_group_rule" "bastion_ssh_ingress" {
 }
 
 resource "aws_security_group_rule" "bastion_icmp_ingress" {
-  type              = "ingress"
-  from_port         = 8
-  to_port           = 0
-  protocol          = "icmp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type      = "ingress"
+  from_port = 8
+  to_port   = 0
+  protocol  = "icmp"
+
+  cidr_blocks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "100.64.0.0/10",
+  ]
+
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.bastion_pub_sg.id}"
 }
 
 resource "aws_security_group_rule" "bastion_traceroute_ingress" {
-  type              = "ingress"
-  from_port         = 33434
-  to_port           = 33534
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type      = "ingress"
+  from_port = 33434
+  to_port   = 33534
+  protocol  = "udp"
+
+  cidr_blocks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "100.64.0.0/10",
+  ]
+
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.bastion_pub_sg.id}"
 }
@@ -101,7 +115,7 @@ resource "aws_security_group_rule" "vyos_udp_4500_ingress" {
   security_group_id = "${aws_security_group.vyos_pub_sg.id}"
 }
 
-resource "aws_security_group_rule" "vyos_all_ingress" {
+resource "aws_security_group_rule" "vyos_ingress" {
   type                     = "ingress"
   from_port                = 0
   to_port                  = 0
@@ -142,21 +156,35 @@ resource "aws_security_group" "ec2_prv_sg" {
 }
 
 resource "aws_security_group_rule" "ec2_prv_icmp_ingress" {
-  type              = "ingress"
-  from_port         = 8
-  to_port           = 0
-  protocol          = "icmp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type      = "ingress"
+  from_port = 8
+  to_port   = 0
+  protocol  = "icmp"
+
+  cidr_blocks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "100.64.0.0/10",
+  ]
+
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.ec2_prv_sg.id}"
 }
 
 resource "aws_security_group_rule" "ec2_prv_traceroute_ingress" {
-  type              = "ingress"
-  from_port         = 33434
-  to_port           = 33534
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type      = "ingress"
+  from_port = 33434
+  to_port   = 33534
+  protocol  = "udp"
+
+  cidr_blocks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "100.64.0.0/10",
+  ]
+
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.ec2_prv_sg.id}"
 }
@@ -171,21 +199,35 @@ resource "aws_security_group_rule" "ec2_prv_bastion_ingress" {
 }
 
 resource "aws_security_group_rule" "ec2_prv_tcp_dns_ingress" {
-  type              = "ingress"
-  from_port         = 53
-  to_port           = 53
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type      = "ingress"
+  from_port = 53
+  to_port   = 53
+  protocol  = "tcp"
+
+  cidr_blocks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "100.64.0.0/10",
+  ]
+
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.ec2_prv_sg.id}"
 }
 
 resource "aws_security_group_rule" "ec2_prv_udp_dns_ingress" {
-  type              = "ingress"
-  from_port         = 53
-  to_port           = 53
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type      = "ingress"
+  from_port = 53
+  to_port   = 53
+  protocol  = "udp"
+
+  cidr_blocks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "100.64.0.0/10",
+  ]
+
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.ec2_prv_sg.id}"
 }

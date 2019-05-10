11dada105815c7b2826718ed21b03dbdd5fce247
kaysal <salawu@google.com>
2019-05-10 04:44:37 +0100

    remote state name change

--- gcp/cloudtuple/1-host/1-nva/0-vpc/5-peer.sh
+++ gcp/cloudtuple/1-host/1-nva/0-vpc/5-peer.sh
@@ -1,6 +1,20 @@
-gcloud alpha compute networks peerings create host-trust-to-orange \
+gcloud beta compute networks peerings create host-trust-to-orange \
   --network nva-trust \
   --peer-network vpc \
   --peer-project orange-project-c3 \
+  --import-custom-routes \
+  --export-custom-routes
+
+gcloud beta compute networks peerings create host-vpc-to-host-trust \
+  --network vpc \
+  --peer-network nva-trust \
+  --peer-project host-project-39 \
+  --import-custom-routes \
+  --export-custom-routes
+
+gcloud beta compute networks peerings create host-trust-to-host-vpc \
+  --network nva-trust \
+  --peer-network vpc \
+  --peer-project host-project-39 \
   --import-custom-routes \
   --export-custom-routes
